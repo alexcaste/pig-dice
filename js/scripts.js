dd9af7dbfa018af93c0e70fe1d63d7d48c5abb91
alex & jordan <student@epicodus.com>
2015-06-03 12:13:59 -0700

    add a bunch of objects and methods

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,66 @@
-function Player(playerName, playerScore) {
-  this.playerName = playerName;
+function Player(playerId, playerScore) {
+  this.playerId = playerId;
   this.playerScore = playerScore;
 }
 
-function Die(side) {
-  this.side = side;
+function Dice(number) {
+  this.number = number;
+}
+
+// Dice.prototype.roll = function(min, max) {
+//   return Math.floor(Math.random() * (max - min)) + min;
+// }
+//
+// Dice.prototype.diceMax = function(number) {
+//   return (number * 6) + 1;
+// }
+
+Dice.prototype.rollAgain = function(diceRoll) {
+  if (diceRoll != 1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+Dice.prototype.roll = function(numberOfDice) {
+  var die = Math.floor(Math.random() * (6 - 1)) + 1;
+}
+
+function Game(numberOfPlayers, numberOfDice, charlieSheen) {
+  this.numberOfPlayers = numberOfPlayers;
+  this.numberOfDice = numberOfDice;
+  this.charlieSheen = charlieSheen;
+}
+
+Game.prototype.nextPlayer = function(currentPlayerId, numberOfPlayers) {
+  var nextPlayerId  = (currentPlayerId + 1);
+  var numberOfPlayers = this.numberOfPlayers;
+  if (nextPlayerId >= numberOfPlayers) {
+    nextPlayerId = 1;
+  } else {}
+  return nextPlayerId;
+}
+
+Game.prototype.hold = function(currentPlayer, currentScore) {
+  var currentPlayerScore = currentPlayer.playerScore;
+  var newPlayerScore = currentScore + currentPlayerScore;
+  return currentPlayer.playerScore = newPlayerScore;
+}
+
+Game.prototype.createPlayers = function(numberOfPlayers) {
+  var allPlayers = []
+  for(var x = 1; x <= numberOfPlayers; x++) {
+    var player = new Player(x,0);
+    allPlayers.push(player);
+  }
+  return allPlayers;
+}
+
+Game.prototype.activeScore = function(rollAgain, diceRoll, currentScore) {
+  if (rollAgain === false) {
+    return 0;
+  } else {
+  return diceRoll + currentScore;
+  }
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,14 +1,73 @@
 describe('Player', function(){
   it("will create a player with a name with a score of 0", function(){
-    var testPlayer = new Player("John Digweed",0)
-    expect(testPlayer.playerName).to.equal("John Digweed")
-    expect(testPlayer.playerScore).to.equal(0)
+    var testPlayer = new Player(1,0);
+    expect(testPlayer.playerScore).to.equal(0);
+    expect(testPlayer.playerId).to.equal(1);
   });
 });
 
 describe('Die', function(){
-  it("will create a die with a side number", function(){
-    var testDie = new Die(2)
-    expect(testDie.side).to.equal(2)
+  it("will create a number of dice for a game", function(){
+    var testDie = new Dice(2);
+    expect(testDie.number).to.equal(2);
+  });
+
+  it("will return a random number between 1-6", function(){
+    var testDie = new Dice(1);
+    expect("123456").to.contain(testDie.roll(1,7));
+  });
+
+  // it("will the max number that dice can have (excluded)", function(){
+  //   var testDie = new Dice(2);
+  //   expect(testDie.diceMax(testDie.number)).to.equal(13);
+  // });
+
+  it("will return false if a die rolls 1", function(){
+    var testDie = new Dice(1);
+    var diceRoll1 = 1;
+    var diceRoll2 = 3;
+    expect(testDie.rollAgain(diceRoll1)).to.equal(false);
+    expect(testDie.rollAgain(diceRoll2)).to.equal(true);
+  });
+});
+
+describe('Game', function(){
+  it("will add a game object with a number of players, a turn and Winning!", function(){
+    var testPlayer1 = new Player(1, 0);
+    var testGame = new Game(2, 2, false);
+    expect(testGame.numberOfPlayers).to.equal(2);
+    expect(testGame.numberOfDice).to.equal(2);
+    expect(testGame.charlieSheen).to.equal(false);
+  });
+
+  it("will return the player for the next turn", function(){
+    var testPlayer1 = new Player(1, 0);
+    var testPlayer2 = new Player(2,0);
+    var testGame = new Game(2, 2, false);
+    expect(testGame.nextPlayer(testPlayer2.playerId, testGame.numberOfPlayers )).to.equal(testPlayer1.playerId);
+  });
+
+  it("will add a dice roll to the player score and return score", function(){
+    var testDie = new Dice(1);
+    var testPlayer = new Player(1, 0);
+    var testGame = new Game(1, 1, false);
+    var currentScore = testDie.roll(1,7);
+    testGame.hold(testPlayer, currentScore);
+    expect("123456").to.contain(testPlayer.playerScore);
+  });
+
+  it("will create player objects for the number of players", function(){
+    var testGame = new Game(1, 1, false);
+    var testPlayer = new Player(1, 0);
+    expect(testGame.createPlayers(testGame.numberOfPlayers)).to.eql([testPlayer]);
+  });
+
+  it("will add the roll to the temporary score", function(){
+    var testGame = new Game(1, 1, false);
+    var testDie = new Dice(1);
+    var diceRoll = testDie.roll(1,7);
+    var diceRoll2 = 0;
+    expect("123456").to.contain(testGame.activeScore(true, diceRoll, 0));
+    expect(testGame.activeScore(false, diceRoll2, 32)).to.equal(0);
   });
 });
